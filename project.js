aec91bf77d31fc529ed685cde1a0ae2e89446850
jbatman11 <41481220+jbatman11@users.noreply.github.com>
2018-08-02 11:25:18 -0400

    Update project.js

--- project.js
+++ project.js
@@ -1,8 +1,6 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
@@ -12,17 +10,17 @@ function closeNav() {
     document.body.style.backgroundColor = "white";
 }
 
-
-function openLink(evt, animName) {
+function openCity(evt, cityName) {
   var i, x, tablinks;
   x = document.getElementsByClassName("city");
   for (i = 0; i < x.length; i++) {
-     x[i].style.display = "none";
+      x[i].style.display = "none";
   }
   tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < x.length; i++) {
-     tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
+      tablinks[i].className = tablinks[i].className.replace(" w3-red", "");
   }
-  document.getElementById(animName).style.display = "block";
+  document.getElementById(cityName).style.display = "block";
   evt.currentTarget.className += " w3-red";
 }
+
